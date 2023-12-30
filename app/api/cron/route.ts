093789254404e9b7ad2c8f1f61201669b3be30c7
SK-ILLish-GIT <sksahilparvez2000@gmail.com>
2023-12-31 00:10:16 +0530

    try to depoly 1.2

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,6 +11,7 @@ import {
 import { Product } from "@/types";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300;
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
