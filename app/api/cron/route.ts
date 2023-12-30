f606f4d9870d04822e5586a7fad487dc2c501add
SK-ILLish-GIT <sksahilparvez2000@gmail.com>
2023-12-30 22:38:28 +0530

    trying to deploy 1.1

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,6 +11,9 @@ import {
 import { Product } from "@/types";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function GET() {
 	try {
 		connectDB();
